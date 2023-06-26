e1a2946f348214330fcd84307878af396bcf0a0a
AlexFarrellyTPD <123494966+AlexFarrellyTPD@users.noreply.github.com>
2023-06-27 00:55:36 +0100

    Update config.js

--- src/core/config.js
+++ src/core/config.js
@@ -6,9 +6,9 @@ export const SUPPORT_PHONE = "0333 335 0439";
 export const SUPPORT_EMAIL = "hello@tradepricesdirect.com";
 
 export const META_DEFAULTS = {
-  title: "Trade Prices Direct",
+  title: "Construction Materials Direct",
   description:
-    "Buy kitchens, bathrooms, boilers, and more at trade prices direct from the manufacturer.",
+    "Buy kitchens, bathrooms, boilers, and more at Construction Materials Direct from the manufacturer.",
   type: "website",
   url: !ssrMode ? window.location.origin : "",
   "og:image": "/branding/og-image.jpg",
